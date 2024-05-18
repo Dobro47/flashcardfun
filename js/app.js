5dc4cf0997b991726a8cdf2f143cbe449f2dff6b
Dobro47 <77642103+Dobro47@users.noreply.github.com>
2024-05-17 22:10:58 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,38 +1,52 @@
 const term = document.querySelector('.term');
-const definition = document.querySelector('.definition');
+const translation = document.querySelector('.translation');
 const checkButton = document.querySelector('.check');
 const nextButton = document.querySelector('.next');
 
-let words = {
-    Hello: "Ciao",
-    Goodbye: "Ciao",
-    Yes: "Si",
-    No: "No",
-    Please: "Per favore",
-    ["Thank you"]: "Grazie",
-    ["You're welcome"]: "Prego",
-    Cheers: "Salute!",
-    ["Excuse me (for attention)"]: "Scusi",
-    ["Excuse me (to pass by)"]: "Permesso",
-    ["Do you speak English?"]: "Parla Inglese?",
-    ["I dont understand"]: "Non capisco",
-    ["I'm sorry"]: "Mi Dispiace"
-}
 
-let data = Object.entries(words)
+function genText() {
+    let words = {
+        Hello: "Ciao - A popular greeting that can be used for both hello & goodbye",
+        Goodbye: "Ciao",
+        Yes: "Si",
+        No: "No",
+        Please: "Per favore",
+        ["Thank you"]: "Grazie",
+         Aperitivo: "pre-dinner drinks & snacks",
+        Stazione: "train station",
+        ["How much does it cost?"]: "Quanto costa",
+        ["You're welcome"]: "Prego",
+        Cheers: "Salute!",
+        ["Excuse me (for attention)"]: "Scusi",
+        Buonasera: "good evening",
+        Buonanotte: "goodnight- and can be used in both formal and informal settings",
+        ["I dont understand"]: "Non capisco",
+        Salve: "A formal greeting for hello",
+        ["Grazie mille"]: "thank you so much",
+        Buongiorno: "means good day or morning",
+        Bello: "beautiful (masculine)",
+        ["I'm sorry"]: "Mi Dispiace",
+        Buona: "good (feminine)",
+        Birra: "beer",
+        ["Il conto"]: "the check or bill",
+        Vino: "wine",
+        ["Via/Strada"]:"street" 
+    };
+   let data = Object.entries(words);
 
-function getRandomWord(){
+function genText(){
     randomTerm = data[Math.floor(Math.random() * data.length)]
     term.innerHTML = `<h3> ${randomTerm[0]}</h3>`;
-    definition.innerHTML = `<h3> ${randomTerm[1]}</h3`;
+    translation.innerHTML = `<h3> ${randomTerm[1]}</h3`;
 }
 
 
 checkButton.addEventListener('click', function(){
-    definition.style.display = 'block';
+    translation.style.display = 'block';
 });
 
 nextButton.addEventListener('click', function(){
-    getRandomWord();
-    definition.style.display ='none';
+    genText();
+    translation.style.display ='none';
 });
+}
